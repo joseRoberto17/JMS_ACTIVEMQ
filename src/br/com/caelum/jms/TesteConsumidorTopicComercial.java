21e1351af17b4097fd1cb91132c4e3752ecb22d7
Jose Roberto Xavier da Silva <jose.silva1@ibm.com>
2020-10-22 17:43:06 -0300

    Criacao de topico duravel

---
+++ src/br/com/caelum/jms/TesteConsumidorTopicComercial.java
@@ -0,0 +1,66 @@
+package br.com.caelum.jms;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+public class TesteConsumidorTopicComercial {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {
+		//Inicializando
+		InitialContext context = new InitialContext();
+		
+		//lookup eu pego o jndi
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.setClientID("comercial");
+		connection.start();
+		
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		
+		Topic topico = (Topic) context.lookup("loja");
+		
+		MessageConsumer consumer =  session.createDurableSubscriber(topico, "assinatura");
+		// Fim
+		// Uma mensagem Message message = (Message) consumer.receive();
+		
+		consumer.setMessageListener(new MessageListener() {
+
+			@Override
+			public void onMessage(Message message) {
+				
+				TextMessage txtMessage = (TextMessage) message;
+				try {
+					System.out.println(txtMessage.getText());
+				} catch (JMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+   
+			
+		});
+		
+		new Scanner(System.in).nextLine();
+		
+		session.close();
+		connection.close();
+		context.close();
+	}
+
+}
